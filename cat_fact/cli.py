ef750f18efbaa0af3ecd8080025eae1119a40d7e
sohail535 <sohail.bphc@gmail.com>
2019-01-16 22:58:36 +0530

    Add snail, horse and dog facts also :)

--- cat_fact/cli.py
+++ cat_fact/cli.py
@@ -10,11 +10,12 @@ cat_client = CatClient(requests.Session(), "http://cat-fact.herokuapp.com")
 
 
 @click.command()
+@click.option("--animal", default="cat")
 @click.version_option(version=__version__)
-def cli(verbose=False):
-    random_fact = cat_client.get_random_fact()
+def cli(animal, verbose=False):
+    random_fact = cat_client.get_random_fact(animal)
     if not random_fact:
-        click.echo("Unable to get cat facts at the moment :'(")
+        click.echo("Unable to get facts at the moment :'(")
     else:
         click.echo(random_fact["text"])
 
--- cat_fact/client.py
+++ cat_fact/client.py
@@ -6,12 +6,21 @@ class CatClient(object):
     def __init__(self, session, host_url):
         self._session = session
         self._host_url = host_url
+        self.animals = ["cat", "horse", "dog"]
+
+    def animal_exists(self, animal):
+        return animal in self.animals
 
     def rooted_uri(self, *args):
         return posixpath.join(self._host_url, *args)
 
-    def get_random_fact(self):
-        api_end_point = self.rooted_uri("facts/random")
+    def get_random_fact(self, animal):
+        if not self.animal_exists(animal):
+            return None
+        api_end_point = self.rooted_uri(
+            "facts/random",
+            "?animal_type={}".format(animal)
+        )
         response = self._session.get(api_end_point)
         if response.ok:
             return response.json()
